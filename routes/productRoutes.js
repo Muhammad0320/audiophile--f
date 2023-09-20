dbe214f2f591b477b02ca8239b3bf9e4190d8a47
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-20 15:04:11 +0100

    CRUD operations for products is completed and tested

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { createNewProduct } = require('../controllers/productController');
+const {
+  createNewProduct,
+  getAllProducts,
+  updateProduct,
+  getProduct,
+  deleteProduct,
+} = require('../controllers/productController');
 
 const router = express.Router();
 
-router.route('/').post(createNewProduct);
+router.route('/').post(createNewProduct).get(getAllProducts);
+
+router.route('/:id').patch(updateProduct).get(getProduct).delete(deleteProduct);
 
 module.exports = router;
