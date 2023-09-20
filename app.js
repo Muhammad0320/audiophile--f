d96f15624f0a74e436195662b9146ec1a3bb08a5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-20 08:19:20 +0100

    Products Routes implemented in app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const productRoutes = require('./routes/productRoutes');
+
 const app = express();
 
 app.use((req, res, next) => {
   console.log('Hello');
 });
+
+app.use('/', productRoutes);
 
 module.exports = app;
