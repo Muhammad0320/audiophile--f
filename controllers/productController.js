b5ee08f2ac79f87180b7b25e74a47e87e3d7ada2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-20 14:45:43 +0100

    Getting all Products controller implemented

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,9 +7,22 @@ exports.createNewProduct = async (req, res) => {
 
   res.status(201).json({
     status: 'success',
-    results: newUser.length,
     data: {
       newUser,
+    },
+  });
+};
+
+exports.getAllProducts = async (req, res) => {
+  const allProducts = await Product.find();
+
+  res.status(200).json({
+    status: 'success',
+
+    result: allProducts.length,
+
+    data: {
+      allProducts,
     },
   });
 };
