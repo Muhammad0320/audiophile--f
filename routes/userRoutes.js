4e42f733aba7c66b5069772ed284abbb9dd0913b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-21 18:17:04 +0100

    forgotpassword route implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { signUp, login } = require('../controllers/authController');
+const {
+  signUp,
+  login,
+  forgotPassword
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/signup').post(signUp);
+
+router.route('/forgotPassword').post(forgotPassword);
 
 router.route('/login').post(login);
 
