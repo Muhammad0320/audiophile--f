7d2b3a036255d007c2d90521cf8ae5e226ee5c08
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-21 17:27:11 +0100

    send fake emails with mailtraps implemented

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const sendMail = async function(user, mailOption) {
-  const transporter = nodemailer.createTransport();
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    logger: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
 
   const options = {
     from: ` Muhammad Awwal <audiophile@gmail.com> `,
 
-    to: user.email,
-    subject: mailOption.subject
+    to: mailOption.email,
+    subject: mailOption.subject,
+    text: mailOption.message
   };
+
+  await transporter.sendMail(options);
 };
 
 module.exports = sendMail;
