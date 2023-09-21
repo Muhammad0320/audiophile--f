c7b9298d674d6ee452ff8cc2e667823eb0744b2b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-21 21:10:05 +0100

    Added password reset route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const {
   signUp,
   login,
-  forgotPassword
+  forgotPassword,
+  resetPassword
 } = require('../controllers/authController');
 
 const router = express.Router();
@@ -10,6 +11,8 @@ const router = express.Router();
 router.route('/signup').post(signUp);
 
 router.route('/forgotPassword').post(forgotPassword);
+
+router.route('/passwordReset/:token').patch(resetPassword);
 
 router.route('/login').post(login);
 
