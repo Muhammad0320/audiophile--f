b4d5ef1865df1fddf89ac059ed897bb82dd28857
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 04:34:41 +0100

    Added a route for getting the current user without getting id from the params

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 const { getAll, getOne, updateOne } = require('./handlerFactory');
 
 exports.getAllUsers = getAll(User);
@@ -6,3 +8,20 @@ exports.getAllUsers = getAll(User);
 exports.getUser = getOne(User);
 
 exports.updateUser = updateOne(User);
+
+exports.getMe = catchAsync(async (req, res, next) => {
+  const currentUser = await User.findById(req.user.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError('You are not logged in! Log in to gain access', 401)
+    );
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: currentUser
+    }
+  });
+});
