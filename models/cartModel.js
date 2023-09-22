01899175070aa7d269960194cd35f56dae9b8841
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 13:12:29 +0100

    User and product popolate implemented

--- models/cartModel.js
+++ models/cartModel.js
@@ -22,14 +22,14 @@ const cartSchema = new mongoose.Schema({
   }
 });
 
-// cartSchema.pre(/^find/, function(next) {
-//   this.populate({ path: 'user', select: 'name photo' }).populate({
-//     path: 'product',
-//     select: 'name price category discountPrice'
-//   });
+cartSchema.pre(/^find/, function(next) {
+  this.populate({ path: 'user', select: 'name photo' }).populate({
+    path: 'product',
+    select: 'name price category discountPrice'
+  });
 
-//   next();
-// });
+  next();
+});
 
 const Cart = mongoose.model('Cart', cartSchema);
 
