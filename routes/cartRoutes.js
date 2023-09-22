e04247fd31b5cb3896b7dcf1fcb8ae229c8227ce
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 17:30:37 +0100

    added route to get all carts

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { getAllCarts } = require('../controllers/cartController');
 
 const router = express.Router();
+
+router.route('/').get(getAllCarts);
 
 module.exports = router;
