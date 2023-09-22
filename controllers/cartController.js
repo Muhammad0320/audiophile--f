a00feffff254ff3c3e8939071e5a3801996a922d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 17:24:15 +0100

    Cart update function created

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,10 @@
 const Cart = require('../models/cartModel');
-const { createOne, getAll, getOne } = require('./handlerFactory');
+const { createOne, getAll, getOne, updateOne } = require('./handlerFactory');
 
 exports.addItemToCart = createOne(Cart);
 
 exports.getAllCarts = getAll(Cart);
 
 exports.getCart = getOne(Cart);
+
+exports.updateCart = updateOne(Cart);
