006491e724c3cac350b5e191ee04a2cfc50cd5c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 17:46:52 +0100

    added authentication and authorization to some protected cart routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -7,15 +7,20 @@ const {
   deleteCart,
   getMyCart
 } = require('../controllers/cartController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/myCart').get(getMyCart);
 
+router.use(protect);
+
 router
   .route('/')
-  .get(getAllCarts)
+  .get(restrictTo('admin', 'user'), getAllCarts)
   .post(addItemToCart);
+
+router.use(restrictTo('admin', 'user'));
 
 router
   .route('/id')
