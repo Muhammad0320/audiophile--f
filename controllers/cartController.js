5864814aabd95d83d2b0df7e8c3ba9154283768b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-22 17:40:51 +0100

    Controller to find current users cart is implemented

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,4 +1,6 @@
 const Cart = require('../models/cartModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 const {
   createOne,
   getAll,
@@ -16,3 +18,24 @@ exports.getCart = getOne(Cart);
 exports.updateCart = updateOne(Cart);
 
 exports.deleteCart = deleteOne(Cart);
+
+exports.getMyCart = catchAsync(async (req, res, next) => {
+  const myCart = await Cart.find({ user: req.user.id });
+
+  if (!myCart) {
+    return next(
+      new AppError(
+        'You do not have any item in your cart, start by adding item that catch your attention',
+        404
+      )
+    );
+  }
+
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      cart: myCart
+    }
+  });
+});
