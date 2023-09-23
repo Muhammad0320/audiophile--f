0c174318a25201782a4537176edef4d06b0b0593
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-23 04:15:33 +0100

    Added handler function to get reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,3 +2,5 @@ const Review = require('../models/reviewModel');
 const { createOne } = require('./handlerFactory');
 
 exports.createNewRating = createOne(Review);
+
+exports.getAllRatings = createOne(Review);
