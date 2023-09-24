52d8e18550189367c43fc9f8dfccb4f7c6311879
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-24 11:48:45 +0100

    Implemented headphone data

--- client/src/features/category/CategoryHeadphone.jsx
+++ client/src/features/category/CategoryHeadphone.jsx
@@ -1,21 +1,24 @@
 import { styled } from "styled-components";
-import { fakeData } from "../../service/data";
 
 import Category from "./Category";
 import ContainerHero from "../../ui/ContainerHero";
 import CategoryBox from "../../ui/CategoryBox";
-
-const headphoneData = fakeData.filter((data) => data.category === "headphones");
+import { useProductCategory } from "./useProductCategory";
+import Spinner from "../../ui/Spinner";
 
 const CategoryContainer = styled.div`
   margin: 15rem 0;
 `;
 
 function CategoryHeadphone() {
+  const { categoryProduct = [], isLoading } = useProductCategory("headphones");
+
+  if (isLoading) return <Spinner />;
+
   return (
     <CategoryContainer>
-      {headphoneData.map((data, index) => (
-        <Category key={data.id} categoryData={data} index={index} />
+      {categoryProduct.map((data, index) => (
+        <Category key={data._id} categoryData={data} index={index} />
       ))}
 
       <CategoryBox />
