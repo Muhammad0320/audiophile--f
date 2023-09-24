4d7f61aa581ea3928237a342be185f7792bec816
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-24 10:43:19 +0100

    Added function to get product by id

--- client/src/service/apiProduct.js
+++ client/src/service/apiProduct.js
@@ -4,21 +4,30 @@ export const getAllProducts = async () => {
   try {
     const res = await axios.get(`http://127.0.0.1:3000/api/v1/products`);
 
-    return res;
+    return res.data.data;
   } catch (error) {
-    console.log(error.response.data.message);
+    console.error(error.response.data.message);
   }
 };
 
 export const getProductsByCategory = async (category) => {
-  console.log(category);
   try {
     const res = await axios.get(
       `http://127.0.0.1:3000/api/v1/products?category=${category}`
     );
 
-    return res;
+    return res.data.data;
   } catch (error) {
-    console.log(error.response.data.message);
+    console.error(error.response.data.message);
+  }
+};
+
+export const getProductById = async (id) => {
+  try {
+    const res = await axios.get(`http://127.0.0.1:3000/api/v1/products/${id}`);
+
+    return res.data.data;
+  } catch (error) {
+    console.error(error.response.data.message);
   }
 };
