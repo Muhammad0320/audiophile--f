65d52b2fb84b1833f417bc9855babc6ab0484284
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-24 04:40:37 +0100

    Finished setting up react query in our project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import AppLayout from "./ui/AppLayout";
 import Home from "./pages/Home";
 import Headphone from "./pages/Headphone";
@@ -8,9 +11,17 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Details from "./pages/Details";
 import CheckoutPage from "./pages/CheckoutPage";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -26,7 +37,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
