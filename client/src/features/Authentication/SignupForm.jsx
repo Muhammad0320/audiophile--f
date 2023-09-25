22cce82a2f5e41d0b61d8756bb6c9b798759caf8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 05:36:37 +0100

    Registerd with react-hook-form

--- client/src/features/Authentication/SignupForm.jsx
+++ client/src/features/Authentication/SignupForm.jsx
@@ -2,23 +2,45 @@ import Form2 from "../../ui/Form2";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
+import { useForm } from "react-hook-form";
+
 function SignupForm() {
+  const { register, reset, handleSubmit, formState } = useForm();
+
   return (
     <Form2>
       <FormRow label="name">
-        <Input />
+        <Input
+          id="name"
+          type="text"
+          {...register("name", { required: "User must have a name" })}
+        />
       </FormRow>
 
       <FormRow label="email">
-        <Input />
+        <Input
+          id="email"
+          type="email"
+          {...register("name", { required: " User must have an email " })}
+        />
       </FormRow>
 
       <FormRow label="password">
-        <Input />
+        <Input
+          id="password"
+          type="password"
+          {...register("name", { required: "Please input your password" })}
+        />
       </FormRow>
 
-      <FormRow label="passwordConfirm">
-        <Input />
+      <FormRow label="confirm password">
+        <Input
+          id="passwordConfirm"
+          type="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "confirm your password",
+          })}
+        />
       </FormRow>
     </Form2>
   );
