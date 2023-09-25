826ba4a791f3116778a1c9eb66594a11338ed59c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 05:26:44 +0100

    Implemeted signup form

--- client/src/features/Authentication/SignupForm.jsx
+++ client/src/features/Authentication/SignupForm.jsx
@@ -1,5 +1,27 @@
+import Form2 from "../../ui/Form2";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
 function SignupForm() {
-  return <div></div>;
+  return (
+    <Form2>
+      <FormRow label="name">
+        <Input />
+      </FormRow>
+
+      <FormRow label="email">
+        <Input />
+      </FormRow>
+
+      <FormRow label="password">
+        <Input />
+      </FormRow>
+
+      <FormRow label="passwordConfirm">
+        <Input />
+      </FormRow>
+    </Form2>
+  );
 }
 
 export default SignupForm;
