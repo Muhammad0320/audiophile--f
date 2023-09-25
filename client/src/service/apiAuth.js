fbcdfbd4c9922e9603b2666f937e5f3cfc0de06f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 09:26:36 +0100

    Created a login api for our login page

--- client/src/service/apiAuth.js
+++ client/src/service/apiAuth.js
@@ -21,3 +21,22 @@ export const signupApi = async ({ name, email, password, passwordConfirm }) => {
     toast.error(error.response.data.message);
   }
 };
+
+export const loginApi = async ({ email, password }) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "http://127.0.0.1:3000/api/v1/users/login",
+      data: {
+        email,
+        password,
+      },
+    });
+
+    toast.success("Login successful");
+
+    return res.data;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
