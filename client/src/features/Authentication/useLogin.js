c6516ba4785aa8cb699e01083e882ca34060eff0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 15:24:10 +0100

    Finally able to handle errors with my custom hook

--- client/src/features/Authentication/useLogin.js
+++ client/src/features/Authentication/useLogin.js
@@ -1,9 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginApi } from "../../service/apiAuth";
+import toast from "react-hot-toast";
 
 export const useLogin = () => {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: loginApi,
+
+    onSuccess: () => {
+      toast.success("Logged in successfully");
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
   });
 
   return { login, isLoading };
--- client/src/service/apiAuth.js
+++ client/src/service/apiAuth.js
@@ -23,23 +23,34 @@ export const signupApi = async ({ name, email, password, passwordConfirm }) => {
 };
 
 export const loginApi = async ({ email, password }) => {
+  const res = await axios({
+    method: "POST",
+    url: "http://127.0.0.1:3000/api/v1/users/login",
+    data: {
+      email,
+      password,
+    },
+  });
+
+  toast.success("Login successful");
+
+  // window.location("/home");
+
+  return res.data;
+  //   } catch (error) {
+  //     console.log(error);
+  //     toast.error(error.response.data.message);
+  //   }
+};
+
+export const getCurrentUserApi = async () => {
   try {
-    const res = await axios({
-      method: "POST",
-      url: "http://127.0.0.1:3000/api/v1/users/login",
-      data: {
-        email,
-        password,
-      },
-    });
+    const res = await axios.get("http://127.0.0.1:3000/api/v1/users/me");
 
-    toast.success("Login successful");
-
-    window.location("/home");
+    toast.success("User loaded successfully");
 
     return res.data;
   } catch (error) {
-    console.log(error);
     toast.error(error.response.data.message);
   }
 };
