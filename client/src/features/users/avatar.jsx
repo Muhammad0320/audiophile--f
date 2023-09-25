069b9b619666079c71778d87c3846af813f24f76
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 15:46:47 +0100

    Adding the avatar part of the header

--- client/src/features/users/avatar.jsx
+++ client/src/features/users/avatar.jsx
@@ -1,17 +1,44 @@
 import styled from "styled-components";
+import { useLogin } from "../Authentication/useLogin";
+import { useUser } from "./useUser";
+import Spinner from "../../ui/Spinner";
 
 const StyledAvatar = styled.div`
-  color: var(--color-white);
-  font-size: 1.5rem;
-  text-transform: uppercase;
-
   display: flex;
   justify-content: center;
   align-items: center;
+  color: var(--color-white);
+  font-size: 1.5rem;
+  text-transform: uppercase;
+`;
+
+const ImageContainer = styled.img`
+  display: block;
+  height: 2rem;
+  width: 2rem;
+
+  border-radius: 50%;
+  box-sizing: content-box;
+  transition: border 0.3s ease-in-out;
+
+  &:hover {
+    border: 2px solid var(--color-primary);
+  }
 `;
 
 function Avatar() {
-  return <StyledAvatar></StyledAvatar>;
+  const { user = {}, isLoading } = useUser();
+
+  console.log(user);
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <StyledAvatar>
+      <ImageContainer />
+      <span> Muhammad </span>
+    </StyledAvatar>
+  );
 }
 
 export default Avatar;
