bcb4e28702ecff305891cf401035e00df08837b9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 04:19:58 +0100

    Implemented signup function api call

---
+++ client/src/service/apiAuth.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const signup = async ({ name, email, password, passwordConfirm }) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: ` http://127.0.0.1:3000/api/v1/users/signup`,
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
