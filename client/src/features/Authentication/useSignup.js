6f1bd110433b7f00a1d70d9af565be826edb8e71
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-25 15:32:33 +0100

    Onsuceess and onerror working perfectly

--- client/src/features/Authentication/useSignup.js
+++ client/src/features/Authentication/useSignup.js
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { signupApi } from "../../service/apiAuth";
+import toast from "react-hot-toast";
 
 export const useSignup = () => {
   const {
@@ -8,6 +9,14 @@ export const useSignup = () => {
     error,
   } = useMutation({
     mutationFn: signupApi,
+
+    onSuccess: () => {
+      toast.success("New user created successfully");
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
   });
 
   // console.log(signup);
--- client/src/service/apiAuth.js
+++ client/src/service/apiAuth.js
@@ -1,25 +1,18 @@
 import axios from "axios";
-import toast from "react-hot-toast";
 
 export const signupApi = async ({ name, email, password, passwordConfirm }) => {
-  try {
-    const res = await axios({
-      method: "POST",
-      url: "http://127.0.0.1:3000/api/v1/users/signup",
-      data: {
-        name,
-        email,
-        password,
-        passwordConfirm,
-      },
-    });
+  const res = await axios({
+    method: "POST",
+    url: "http://127.0.0.1:3000/api/v1/users/signup",
+    data: {
+      name,
+      email,
+      password,
+      passwordConfirm,
+    },
+  });
 
-    toast.success("New user successfully created, check your email to confirm");
-
-    return res.data;
-  } catch (error) {
-    toast.error(error.response.data.message);
-  }
+  return res.data;
 };
 
 export const loginApi = async ({ email, password }) => {
@@ -32,25 +25,11 @@ export const loginApi = async ({ email, password }) => {
     },
   });
 
-  toast.success("Login successful");
-
-  // window.location("/home");
-
   return res.data;
-  //   } catch (error) {
-  //     console.log(error);
-  //     toast.error(error.response.data.message);
-  //   }
 };
 
 export const getCurrentUserApi = async () => {
-  try {
-    const res = await axios.get("http://127.0.0.1:3000/api/v1/users/me");
+  const res = await axios.get("http://127.0.0.1:3000/api/v1/users/me");
 
-    toast.success("User loaded successfully");
-
-    return res.data;
-  } catch (error) {
-    toast.error(error.response.data.message);
-  }
+  return res.data;
 };
