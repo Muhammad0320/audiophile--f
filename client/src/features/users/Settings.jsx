84f14cd0224c6c133f9f5c33ab1b4b7111eb9fdc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-26 17:12:29 +0100

    Registered fields with react-hook-form

--- client/src/features/users/Settings.jsx
+++ client/src/features/users/Settings.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Form2 from "../../ui/Form2";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useForm } from "react-hook-form";
 
 const InfoDetails = styled.div`
   padding: 5rem 6rem;
@@ -9,15 +10,23 @@ const InfoDetails = styled.div`
 `;
 
 function Settings() {
+  const { register, handleSubmit } = useForm();
+
   return (
     <InfoDetails>
       <Form2>
         <FormRow account label="Name">
-          <Input account="true" type="text" />
+          <Input
+            account="true"
+            type="text"
+            {...register("name", {
+              required: "This field is required",
+            })}
+          />
         </FormRow>
 
         <FormRow account label="Email address">
-          <Input account="true" type="text" />
+          <Input account="true" type="text" {...register("email")} />
         </FormRow>
       </Form2>
     </InfoDetails>
