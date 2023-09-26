3a5291b56d59893ebe873b3b6829337b90d236e2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-26 13:42:21 +0100

    Designed sidebar

--- client/src/features/users/userDetailPage.jsx
+++ client/src/features/users/userDetailPage.jsx
@@ -1,13 +1,25 @@
 import { styled } from "styled-components";
 
-const pageContainer = styled.div`
+const PageContainer = styled.div`
   background-color: var(--color-white);
 
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
   justify-content: center;
-  align-content: center;
+  align-items: center;
 `;
 
-const detailsContainer = styled.div`
+const DetailsContainer = styled.div`
   background-color: var(--color-white-1);
+  padding: 2rem 4rem;
+  box-shadow: var(--box-shadow);
+`;
+
+const NavList = styled.ul`
+  color: var(--color-white);
+`;
+
+const Sidebar = styled.div`
+  background-color: var(--color-primary);
+  padding: 2rem 3rem;
 `;
