398c80e6da74d9b5d237e552a16b830da5408114
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-26 16:50:47 +0100

    Created settings template

---
+++ client/src/features/users/Settings.jsx
@@ -0,0 +1,26 @@
+import Form2 from "../../ui/Form2";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
+const InfoDetails = styled.div`
+  padding: 2rem 3rem;
+  grid-column: 2 / -1;
+`;
+
+function Settings() {
+  return (
+    <InfoDetails>
+      <Form2>
+        <FormRow label="Name">
+          <Input type="text" />
+        </FormRow>
+
+        <FormRow label="Email address">
+          <Input type="text" />
+        </FormRow>
+      </Form2>
+    </InfoDetails>
+  );
+}
+
+export default Settings;
