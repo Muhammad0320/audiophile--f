a20f5374f0f125bbb800672fe347111f85304ef5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 15:48:33 +0100

    Created a custom hook to get current user review

--- client/src/features/reviews/useGetUserReviews.js
+++ client/src/features/reviews/useGetUserReviews.js
@@ -1,1 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUserReview } from "../../service/apiReviews";
 
+export const useGetUserReview = () => {
+  const { data: reviews, isLoading } = useQuery({
+    queryFn: getCurrentUserReview,
+
+    queryKey: ["review"],
+  });
+
+  return { reviews, isLoading };
+};
