389d9cb10d48af0b149e109d210cb1dcb6cfc193
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 12:35:55 +0100

    Styled common row element

--- client/src/ui/Table.jsx
+++ client/src/ui/Table.jsx
@@ -4,4 +4,24 @@ const TableContainer = styled.div`
   margin: 2rem 0;
 
   border: 1px solid var(--color-dark-2);
+
+  background-color: var(--color-white-2);
+
+  font-size: 1.5rem;
+
+  color: var(--color-dark);
 `;
+
+const CommonRow = styled.div`
+  display: grid;
+  grid-template-columns: ${(props) => props.column};
+  align-content: center;
+
+  column-gap: 2rem;
+`;
+
+// const TableHeader = styled(CommonRow)`
+
+//     padding: ;
+
+// `
