189a75f63dca1b5c99310bde6503562ab869631e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 14:36:00 +0100

    Implemented reviewItem compoenet

--- client/src/features/reviews/ReviewItem.jsx
+++ client/src/features/reviews/ReviewItem.jsx
@@ -1,5 +1,38 @@
-function ReviewItem() {
-  return <div></div>;
+import styled from "styled-components";
+import Table from "../../ui/Table";
+
+const Image = styled.img`
+  display: inline-block;
+  height: 6rem;
+  width: 6rem;
+`;
+
+const Review = styled.span`
+  font-size: 1.2rem;
+
+  font-size: 500;
+`;
+
+const Name = styled.span`
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+function ReviewItem({ data }) {
+  const {
+    review,
+    rating,
+    product: { image, name },
+  } = data || {};
+
+  return (
+    <Table.Row>
+      <Image src={image} />
+      <Name> {name} </Name>
+      <Review> {review} </Review>
+      <span> {rating} </span>
+    </Table.Row>
+  );
 }
 
 export default ReviewItem;
