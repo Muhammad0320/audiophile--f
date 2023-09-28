0b510a7e7e5db7b76d256ef04370dad9aab59913
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 15:42:49 +0100

    Created an axios call to get current users reviews

--- client/src/service/apiReviews.js
+++ client/src/service/apiReviews.js
@@ -1,1 +1,9 @@
-const getCurrentUserReview = () => {};
+import axios from "axios";
+
+export const getCurrentUserReview = () => {
+  const res = axios.get("http://127.0.0.1:3000/api/v1/products//my-reviews", {
+    withCredentials: true,
+  });
+
+  return res.data;
+};
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -32,5 +32,3 @@ router
   .delete(deleteReview);
 
 module.exports = router;
-
-
