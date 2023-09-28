9ebf4c73ebdedd8278e9ef5fefb3dab838032b54
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 15:58:43 +0100

    Implemetation of review table completed

--- client/src/features/reviews/ReviewTable.jsx
+++ client/src/features/reviews/ReviewTable.jsx
@@ -1,6 +1,11 @@
+import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
+import ReviewItem from "./ReviewItem";
+import { useGetUserReview } from "./useGetUserReviews";
 
 function ReviewTable() {
+  const { reviews, isLoading } = useGetUserReview();
+
   return (
     <Table column="0.9fr 1fr 2fr 1.2fr 1.4fr">
       <Table.Header>
@@ -10,6 +15,15 @@ function ReviewTable() {
         <div>Rating</div>
         <div></div>
       </Table.Header>
+
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Table.Body
+          data={reviews}
+          render={(review) => <ReviewItem data={review} key={review._id} />}
+        />
+      )}
     </Table>
   );
 }
