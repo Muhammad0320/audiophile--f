50fb2835be3caa89a490fdb209a2370f60f82508
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-28 14:11:21 +0100

    Started implementing review table

---
+++ client/src/features/reviews/ReviewTable.jsx
@@ -0,0 +1,13 @@
+import Table from "../../ui/Table";
+
+function ReviewTable() {
+  return (
+    <Table column="0.9fr 1fr 2fr 1.2fr 1fr">
+      <Table.Header>
+        <div></div>
+      </Table.Header>
+    </Table>
+  );
+}
+
+export default ReviewTable;
