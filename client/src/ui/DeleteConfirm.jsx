bc8c25838b801be52b2289f895adf58964c7cf4f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-29 12:29:59 +0100

    Added Styled for delete confirm

--- client/src/ui/DeleteConfirm.jsx
+++ client/src/ui/DeleteConfirm.jsx
@@ -1,3 +1,35 @@
+import styled from "styled-components";
+
+const StyledConfirm = styled.p`
+  background-color: var(--color-white);
+
+  border-radius: 1.2rem;
+
+  box-shadow: var(--box-shadow-light);
+
+  font-size: 1.4rem;
+
+  width: 40%;
+
+  padding: 2rem 2.6rem;
+
+  color: var(--color-black);
+
+  span {
+    font-weight: 600;
+    font-size: 1.5rem;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+
+  justify-content: flex-end;
+  align-items: center;
+
+  column-gap: 2rem;
+`;
+
 function DeleteConfirm() {
   return <div></div>;
 }
