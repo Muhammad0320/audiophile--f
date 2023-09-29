6239c84dd9f4b28aacadf0568e412fd30fb8a1a9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-29 08:33:49 +0100

    Created menu context

--- client/src/ui/Menu.jsx
+++ client/src/ui/Menu.jsx
@@ -1,2 +1,24 @@
+import { createContext, useState } from "react";
 
+const MenuContext = createContext();
 
+const Menu = ({ children }) => {
+  const [isOpen, setIsOpen] = useState("");
+
+  const [position, setPosition] = useState({});
+
+  const open = setIsOpen;
+
+  const close = setIsOpen("");
+
+  return (
+    <MenuContext.Provider
+      value={{ isOpen, position, setPosition, open, close }}
+    >
+      {" "}
+      {children}{" "}
+    </MenuContext.Provider>
+  );
+};
+
+export default Menu;
