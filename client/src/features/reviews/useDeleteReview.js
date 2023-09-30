c16df15ad5c81d31e9abef21f2f284882834856c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-30 12:13:10 +0100

    Created a custom hook to delete a review

---
+++ client/src/features/reviews/useDeleteReview.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteReviewApi } from "../../service/apiReviews";
+import toast from "react-hot-toast";
+
+export const useDeleteReview = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteReview, isDeleting } = useMutation({
+    mutationFn: deleteReviewApi,
+
+    onSuccess: () => {
+      toast.success("Review successfully deleted");
+
+      queryClient.invalidateQueries({ active: true });
+    },
+
+    onError: () => {
+      toast.error(
+        "An error occured while deleting this review, please try again"
+      );
+    },
+  });
+
+  return { deleteReview, isDeleting };
+};
