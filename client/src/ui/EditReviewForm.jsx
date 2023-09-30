11520f27d87d7d5535b317738dec0eab01855b50
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-09-30 13:53:36 +0100

    implemeted function for updating review

--- client/src/ui/EditReviewForm.jsx
+++ client/src/ui/EditReviewForm.jsx
@@ -3,6 +3,9 @@ import Form2 from "./Form2";
 import FormRow from "./FormRow";
 import Input from "./Input";
 import { useForm } from "react-hook-form";
+import { useUpdateReview } from "../features/reviews/useUpdateReview";
+import Button from "./Button";
+import SpinnerMini from "./SpinnerMini";
 
 const EditFormContainer = styled.div`
   background-color: var(--color-white);
@@ -14,16 +17,33 @@ const EditFormContainer = styled.div`
   padding: 2.5rem 3rem;
 `;
 
-function EditReviewForm({ review }) {
-  const { reset, register, handleSubmit } = useForm({ defaultValues: review });
+function EditReviewForm({ review, onClose }) {
+  const { id, ...otherfields } = review;
+
+  const { reset, register, handleSubmit } = useForm({
+    defaultValues: otherfields,
+  });
+
+  const { updateReview, isUpdating } = useUpdateReview();
 
   const OnSubmit = ({ rating, review }, e) => {
     e.preventDefault();
+
+    updateReview(
+      { id, data: { rating, review } },
+      {
+        onSettled: () => {
+          onClose?.();
+
+          reset();
+        },
+      }
+    );
   };
 
   return (
     <EditFormContainer>
-      <Form2 onSubmit={handleSubmit()}>
+      <Form2 onSubmit={handleSubmit(OnSubmit)}>
         {/*  For Now  */}
         <FormRow label="Rating">
           <Input
@@ -38,6 +58,17 @@ function EditReviewForm({ review }) {
             {...register("review", { required: "This field is required" })}
           />
         </FormRow>
+
+        <Button disabled={isUpdating} withspinner={isUpdating ? "true" : ""}>
+          {isUpdating ? (
+            <>
+              {" "}
+              <SpinnerMini /> <span> updating... </span>{" "}
+            </>
+          ) : (
+            <span> save update </span>
+          )}
+        </Button>
       </Form2>
     </EditFormContainer>
   );
