9464c0ada3ae3a2a4079d78244b02f14caabb2b4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-01 16:35:46 +0100

    Completed updating cart

--- client/src/features/cart/useCreateNewCartItemOnUser.js
+++ client/src/features/cart/useCreateNewCartItemOnUser.js
@@ -1,1 +1,23 @@
-const useCreateNewCartItemOnUser = () => {};
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createNewCartItemOnUser } from "../../service/apiCart";
+import toast from "react-hot-toast";
+
+export const useCreateNewCartItemOnUser = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateCart, isLoading: isUpdatingCart } = useMutation({
+    mutationFn: createNewCartItemOnUser,
+
+    onSuccess: () => {
+      toast.success("Item persisted to cart successfully");
+
+      queryClient.invalidateQueries({ active: true });
+    },
+
+    onError: () => {
+      toast.error("An error occured while updating the cart ");
+    },
+  });
+
+  return { updateCart, isUpdatingCart };
+};
