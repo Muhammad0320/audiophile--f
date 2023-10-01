7f1c5be54c78354db6bb61dec90ee89fd9e493ce
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-01 12:29:18 +0100

    Still changing the cart slice to add new item

--- client/src/features/cart/cartSlice.js
+++ client/src/features/cart/cartSlice.js
@@ -15,7 +15,7 @@ const cartSlice = createSlice({
     },
 
     addItem(state, action) {
-      return { ...state, cart: [...state.cart, action.payload] };
+      return { ...state, cart: [...(state.cart || []), action.payload] };
     },
 
     deleteItem(state, action) {
