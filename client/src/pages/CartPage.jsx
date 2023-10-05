2b1258c2c89a88a9defc8bf6509cc50fa5cf917f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-05 14:59:29 +0100

    Completed cart page

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,4 +1,17 @@
+import styled from "styled-components";
+import CartTable from "../features/cart/CartTable";
 
+const CartContainer = styled.div`
+  grid-column: 2 / -1;
+  overflow: auto;
+`;
 
+function CartPage() {
+  return (
+    <CartContainer>
+      <CartTable />
+    </CartContainer>
+  );
+}
 
-
+export default CartPage;
