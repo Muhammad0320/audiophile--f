c690cd9cdc250954e61472f7f1072f1b3134df1a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-11 05:00:35 +0100

    feat: add order model

---
+++ server/models/orderModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: [true, 'Order must have a product']
+      },
+
+      price: {
+        type: Number,
+        required: [true, 'A product must have a price']
+      },
+
+      quantity: {
+        type: Number,
+        default: 1
+      }
+    }
+  ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Order must belog to a user']
+  }
+});
