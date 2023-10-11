df22a026bec01fd4158a34c788583dd124403887
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-11 05:13:00 +0100

    chore: add fucntion to get one order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,8 +1,10 @@
 const Order = require('../models/orderModel');
-const { createOne, updateOne, getAll } = require('./handlerFactory');
+const { createOne, updateOne, getAll, getOne } = require('./handlerFactory');
 
 exports.createOrder = createOne(Order);
 
 exports.updateOrder = updateOne(Order);
 
 exports.getAllOrders = getAll(Order);
+
+exports.getOrder = getOne(Order);
