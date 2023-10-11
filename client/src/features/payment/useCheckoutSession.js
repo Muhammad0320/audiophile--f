aed4ca0ea2cb6f36bca2bf74b154854a6af8da95
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-11 15:51:36 +0100

    chore: create  custom hook for stripe checkout

--- client/src/features/payment/useCheckoutSession.js
+++ client/src/features/payment/useCheckoutSession.js
@@ -1,1 +1,16 @@
-const useCheckoutSession = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getCheckoutSesionApi } from "../../service/apiOrder";
+import toast from "react-hot-toast";
+
+export const useCheckoutSession = () => {
+  const { data: checkout, isLoading } = useQuery({
+    queryFn: getCheckoutSesionApi,
+    queryKey: ["checkout"],
+
+    onError: () => {
+      toast.error("Something went very wrong while checking out, Try again!");
+    },
+  });
+
+  return { checkout, isLoading };
+};
