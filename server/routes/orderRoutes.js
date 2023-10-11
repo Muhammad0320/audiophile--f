8331bd5664b42982edadc7004574339fcd5d7874
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-11 05:20:34 +0100

    chore: add routes for order CRUD operations

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const {
+  getAllOrders,
+  createOrder,
+  getOrder,
+  updateOrder,
+  deleteOrder
+} = require('../controllers/orderController');
+const { protect } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.use(protect);
+
+router
+  .route('/')
+  .get(getAllOrders)
+  .post(createOrder);
+
+router
+  .route('/:id')
+  .get(getOrder)
+  .patch(updateOrder)
+  .delete(deleteOrder);
 
 module.exports = router;
