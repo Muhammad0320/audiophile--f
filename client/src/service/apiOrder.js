f9202e660ee3f04608d06c158beba595237ad863
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-11 15:44:31 +0100

    feat: add checkout session function to frontend

--- client/src/service/apiOrder.js
+++ client/src/service/apiOrder.js
@@ -1,1 +1,18 @@
-const getCheckoutSesionApi = () => {};
+/* eslint-disable */
+
+import axios from "axios";
+
+const stripe = Stripe(
+  "pk_test_51Nzip2JNj9gLI0ylAhOBBa0IHTNj2vg4S4ZMA92roceGfNLyhoaYbGyJt1PtXoEP6lpV9KJBeea3KDtov6iK6paP00fvmoVVeY"
+);
+
+export const getCheckoutSesionApi = async () => {
+  const session = await axios.get(
+    "http://127.0.0.1:3000/api/v1/orders/checkout-session",
+    { withCredentials: true }
+  );
+
+  await stripe.redirectToCheckout({
+    sessionId: session.data.session.id,
+  });
+};
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -63,7 +63,7 @@ exports.getCheckoutSesion = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
-    data: session
+    session
   });
 });
 
