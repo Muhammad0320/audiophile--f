0ff0a908af252c0fb1e3ec4a6cd5548807f346bf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-12 06:25:31 +0100

    chore: create order custom hook

--- client/src/features/payment/useCreateOrder.js
+++ client/src/features/payment/useCreateOrder.js
@@ -1,5 +1,22 @@
-function useCreateOrder() {
-  return <div></div>;
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export function useCreateOrder() {
+  const { mutate: createOrder } = useMutation({
+    mutationFn: useCreateOrder,
+
+    onSuccess: () => {
+      toast.success("Your order is successfully placed");
+    },
+
+    onError: () => {
+      toast.error(
+        "An error occured while placing your order, please try again"
+      );
+    },
+  });
+
+  return { createOrder };
 }
 
 export default useCreateOrder;
