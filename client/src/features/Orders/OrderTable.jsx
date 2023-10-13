5f78d145740cdaa934a40f3274224eb0a0f71702
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-13 11:35:01 +0100

    chore: add order table and table header

--- client/src/features/Orders/OrderTable.jsx
+++ client/src/features/Orders/OrderTable.jsx
@@ -1,5 +1,25 @@
+import Spinner from "../../ui/Spinner";
+import Table from "../../ui/Table";
+import { useGetCurrentUserOrder } from "./useGetCurrentUserOrder";
+
 function OrderTable() {
-  return <div></div>;
+  const { myOrder, isLoading } = useGetCurrentUserOrder();
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Table column=".4fr .2fr 1.2fr .4fr .5fr .4fr .3fr" role="table">
+      <Table.Header>
+        <div> Order ID </div>
+        <div> No of product(s) </div>
+        <div> Product info </div>
+        <div> Status </div>
+        <div> Total </div>
+        <div> Date </div>
+        <div>Actio</div>
+      </Table.Header>
+    </Table>
+  );
 }
 
 export default OrderTable;
