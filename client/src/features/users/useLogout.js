43c30a715663d6cd1a888182a917f035db3bec14
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-15 10:48:45 +0100

    chore: create custom hook to strigger logout

--- client/src/features/users/useLogout.js
+++ client/src/features/users/useLogout.js
@@ -1,1 +1,11 @@
-export const useLogout = () => {};
+import { useMutation } from "@tanstack/react-query";
+import { logoutApi } from "../../service/apiAuth";
+
+export const useLogout = () => {
+  const { mutate: logout, isLoading: isLoggingOut } = useMutation({
+    mutationFn: logoutApi,
+    mutationKey: ["user"],
+  });
+
+  return { logout, isLoggingOut };
+};
