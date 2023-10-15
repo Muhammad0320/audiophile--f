c2b0c1e517ececdfa4924a5d4cd45ca79a3f3c4e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-15 10:13:10 +0100

    chore: add verify token to review routes

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -9,11 +9,15 @@ const {
   getReviewsOnProduct,
   getReviewOnUser
 } = require('../controllers/reviewController');
-const { protect, restrictTo } = require('../controllers/authController');
+const {
+  protect,
+  restrictTo,
+  verifyToken
+} = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-router.use(protect);
+router.use(verifyToken, protect);
 
 router.route('/my-reviews').get(getReviewOnUser);
 
