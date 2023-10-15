ec63aeb9b426af1f8c65beab2d961663ca432040
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-15 10:17:07 +0100

    chore: changes the home component to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import HomePage from "../features/HomePage";
+import CategoryBox from "../ui/CategoryBox";
+import ContainerContent from "../ui/ContainerContent";
+import ContainerHero from "../ui/ContainerHero";
+import ContainerHeroImages from "../ui/ContainerHeroImages";
+import ContainerLayout from "../ui/ContainerLayout";
+import Footer from "../ui/Footer";
+import Header from "../ui/Header";
 
-function Home() {
-  return <HomePage />;
+function HomePage() {
+  return (
+    <ContainerLayout>
+      <Header home={true} />
+
+      <ContainerContent>
+        <CategoryBox />
+
+        <ContainerHeroImages />
+        <ContainerHero />
+      </ContainerContent>
+
+      <Footer />
+    </ContainerLayout>
+  );
 }
 
-export default Home;
+export default HomePage;
