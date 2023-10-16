bc70ad7ced06f9056972e20b19371610cf63874b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-16 14:57:24 +0100

    chore: add secure flag to cookie option

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -2,4 +2,5 @@ export const SHIPPING_FEE = 50;
 
 export const VAT = 0.15;
 
-export const SERVER_ROOT_URL = "http://127.0.0.1:3000/api/v1";
+export const SERVER_ROOT_URL =
+  "https://audiophile-muhammad0320.onrender.com/api/v1";
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,7 +37,8 @@ const sendJwt = (res, user, req) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    secure: true
   };
 
   res.cookie('jwt', token, cookieOptions);
