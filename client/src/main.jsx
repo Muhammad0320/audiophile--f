308cb68a496a6e5354d3ff969c830304b8102cb1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-20 20:50:41 +0100

    chore: add instance to scroll to top in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "../store.js";
+import ScrollToTop from "./utils/ScrollToTop.js";
+import { Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <ScrollToTop />
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
--- client/src/utils/ScrollToTop.js
+++ client/src/utils/ScrollToTop.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname = "" } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
