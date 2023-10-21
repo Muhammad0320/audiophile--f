c7bf3a216cc56f8cb91c7e660c8805e6850a9a6b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-21 10:44:19 +0100

    chore: chnage the cors origin

--- client/src/utils/ScrollToTop.jsx
+++ client/src/utils/ScrollToTop.jsx
@@ -4,8 +4,6 @@ import { useLocation } from "react-router-dom";
 function ScrollToTop() {
   const location = useLocation();
 
-  console.log(location);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
