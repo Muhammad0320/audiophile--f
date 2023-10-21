959464bd6b9f77a854c612841ebc77b709590e0c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-21 14:59:09 +0100

    chore: set same site attribute to none

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,8 +38,8 @@ const sendJwt = (res, user, req) => {
       Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true
-    // sameSite: 'Lax'
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'None'
   };
 
   res.cookie('jwt', token, cookieOptions);
