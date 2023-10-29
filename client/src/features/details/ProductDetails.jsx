5a4bca9df48835f7c291607061818f38f344ebbf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-29 14:53:39 +0100

    refactor the review to it component

--- client/src/features/details/ProductDetails.jsx
+++ client/src/features/details/ProductDetails.jsx
@@ -42,38 +42,10 @@ import {
   OtherTextBox,
   OthersContainer,
 } from "./OtherProducts";
+import { StyledReviewCard } from "./Reviews";
 
 const ProductContainer = styled.div`
   margin: ${() => clampBuilder(320, 1200, 4, 5.5)} 0;
-`;
-
-const StyledReviewCard = styled.div`
-  display: grid;
-
-  grid-auto-flow: column;
-
-  grid-auto-columns: 28%;
-
-  padding: 0 1rem;
-
-  column-gap: ${() => clampBuilder(320, 1200, 2.5, 4)};
-
-  overflow-x: auto;
-
-  scroll-snap-type: x mandatory;
-
-  scroll-padding: ${() => clampBuilder(320, 1200, 2.5, 4)};
-
-  padding-bottom: ${() => clampBuilder(320, 1200, 2.5, 4)};
-  margin-bottom: ${() => clampBuilder(320, 1200, 2.8, 5.5)};
-
-  @media (max-width: 920px) {
-    grid-auto-columns: 37%;
-  }
-
-  @media (max-width: 500px) {
-    grid-auto-columns: 43%;
-  }
 `;
 
 function ProductDetails() {
---
+++ client/src/features/details/Reviews.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { clampBuilder } from "../../styles/clampFunction";
+
+export const StyledReviewCard = styled.div`
+  display: grid;
+
+  grid-auto-flow: column;
+
+  grid-auto-columns: 28%;
+
+  padding: 0 1rem;
+
+  column-gap: ${() => clampBuilder(320, 1200, 2.5, 4)};
+
+  overflow-x: auto;
+
+  scroll-snap-type: x mandatory;
+
+  scroll-padding: ${() => clampBuilder(320, 1200, 2.5, 4)};
+
+  padding-bottom: ${() => clampBuilder(320, 1200, 2.5, 4)};
+  margin-bottom: ${() => clampBuilder(320, 1200, 2.8, 5.5)};
+
+  @media (max-width: 920px) {
+    grid-auto-columns: 37%;
+  }
+
+  @media (max-width: 500px) {
+    grid-auto-columns: 43%;
+  }
+`;
