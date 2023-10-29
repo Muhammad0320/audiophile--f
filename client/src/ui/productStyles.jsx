26a4bc88655cd54fa6a30440279c5c1ea96ac119
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-29 05:01:15 +0100

    refactor: add all the  image styles to different component

--- client/src/ui/productStyles.jsx
+++ client/src/ui/productStyles.jsx
@@ -1,11 +1,5 @@
-import { css, styled } from "styled-components";
-import Button from "../../ui/Button";
-import { useNavigate } from "react-router-dom";
-import SmallButton from "../../ui/SmallButton";
-import { useMoveBack } from "../../hooks/useMoveBack";
-import { clampBuilder } from "../../styles/clampFunction";
-import { useViewport } from "../../context/ViewPort";
-import { Text } from "../../ui/Text";
+import styled from "styled-components";
+import { clampBuilder } from "../styles/clampFunction";
 
 export const Container = styled.div`
   display: grid;
@@ -116,45 +110,3 @@ export const ProductPrice = styled.p`
   color: var(--color-dark);
   margin: 0;
 `;
-
-function Category({ categoryData, index }) {
-  const { image, new: isNew, name, description, slug } = categoryData;
-
-  const navigate = useNavigate();
-
-  const moveback = useMoveBack();
-
-  const src = image.startsWith("https") ? image : `/assets/product/${image}`;
-
-  const { viewportWidth } = useViewport();
-
-  return (
-    <>
-      <SmallButton onClick={moveback}> Go back </SmallButton>
-      <Container>
-        <ImageContainer
-          style={{ gridColumn: index % 2 === 0 ? "1 / 2" : "2 / -1" }}
-        >
-          <img src={src} alt=" Product" />
-        </ImageContainer>
-
-        <DescriptionContainer
-          style={{
-            gridColumn: index % 2 === 0 ? "2 / -1" : "1 / 2",
-            gridRow: viewportWidth <= 920 ? "2 / -1" : "1 / 2",
-          }}
-        >
-          {isNew && <NewProduct> New product </NewProduct>}
-          <ProductName> {name} </ProductName>
-          <Text> {description} </Text>
-          <Button onClick={() => navigate(`/product/${slug}`)}>
-            {" "}
-            See product{" "}
-          </Button>
-        </DescriptionContainer>
-      </Container>
-    </>
-  );
-}
-
-export default Category;
