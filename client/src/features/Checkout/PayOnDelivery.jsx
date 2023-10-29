cd98c9062697f3323134a81721a3ddb743c87525
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-29 05:07:11 +0100

    chore: update the pay on delivery component

--- client/src/features/Checkout/PayOnDelivery.jsx
+++ client/src/features/Checkout/PayOnDelivery.jsx
@@ -2,12 +2,13 @@ import { styled } from "styled-components";
 import SVG from "react-inlinesvg";
 import { IconMoney } from "../../ui/Icons";
 import { Text } from "../../ui/Text";
+import { clampBuilder } from "../../styles/clampFunction";
 
 const StyledContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  column-gap: 5rem;
+  column-gap: ${() => clampBuilder(320, 1200, 2, 5)};
 `;
 
 const IconContainer = styled.div`
