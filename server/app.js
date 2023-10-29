bc87f08993164956650db4f4984419d48b37c643
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-29 04:20:41 +0100

    chore: update the cors policy

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.use(express.json());
 // http://127.0.0.1:3000
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173/');
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 
 app.use(
   cors({
-    origin: ['https://audiophile-f.vercel.app', 'http://127.0.0.1:5173/'],
+    origin: ['https://audiophile-f.vercel.app', 'http://127.0.0.1:5173'],
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH', 'DELETE'],
     credentials: true
   })
