3de0e703a28ceeac972a4eabb346dc75f1fa09c3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-30 09:20:54 +0100

    chore: seettings info refactored

--- client/src/features/users/Settings.jsx
+++ client/src/features/users/Settings.jsx
@@ -1,15 +1,10 @@
-import Form2 from "../../ui/Form2";
-import Input from "../../ui/Input";
-import styled from "styled-components";
-import FormRow from "../../ui/FormRow";
-import { useForm } from "react-hook-form";
-import Button from "../../ui/Button";
 import { useUser } from "./useUser";
-import SpinnerMini from "../../ui/SpinnerMini";
-import { useUpdatePassword } from "./useUpdatePassword";
 import Spinner from "../../ui/Spinner";
-import { clampBuilder } from "../../styles/clampFunction";
+import styled from "styled-components";
+import PaswordSettings from "./PaswordSettings";
 import UserDataSettings from "./userDataSettings";
+import DeleteUserAccount from "./DeleteUserAccount";
+import { clampBuilder } from "../../styles/clampFunction";
 
 const InfoDetails = styled.div`
   padding: ${() => clampBuilder(320, 1200, 3, 5)};
@@ -34,100 +29,16 @@ const InfoDetails = styled.div`
   }
 `;
 
-const DangerousOperation = styled.section`
-  padding-top: ${() => clampBuilder(320, 1200, 5, 8)};
-
-  display: flex;
-  flex-direction: column;
-
-  row-gap: ${() => clampBuilder(320, 1200, 1, 2)};
-
-  color: var(--color-red-light);
-
-  font-weight: 600;
-`;
-
 function Settings() {
   const { user = {}, isLoading } = useUser();
 
-  const { updatePassword, isUpdatingPassowrd } = useUpdatePassword();
-
-  const { register, handleSubmit, reset } = useForm();
-
   if (isLoading) return <Spinner />;
-
-  const onSubmitPassword = (
-    { password, passwordConfirm, currentPassword },
-    e
-  ) => {
-    e.preventDefault();
-
-    updatePassword(
-      { currentPassword, password, passwordConfirm },
-      {
-        onSettled: () => reset(),
-      }
-    );
-  };
 
   return (
     <InfoDetails>
       <UserDataSettings user={user} />
-
-      <Form2 onSubmit={handleSubmit(onSubmitPassword)}>
-        <FormRow account label="Current password">
-          <Input
-            type="password"
-            disabled={isUpdatingPassowrd}
-            placeholder="••••••••"
-            account="true"
-            {...register("currentPassword")}
-          />
-        </FormRow>
-
-        <FormRow account label="Password">
-          <Input
-            type="password"
-            disabled={isUpdatingPassowrd}
-            placeholder="••••••••"
-            account="true"
-            {...register("password")}
-          />
-        </FormRow>
-
-        <FormRow account label="Confirm password">
-          <Input
-            type="password"
-            placeholder="••••••••"
-            disabled={isUpdatingPassowrd}
-            account="true"
-            {...register("passwordConfirm")}
-          />
-        </FormRow>
-
-        {isUpdatingPassowrd ? (
-          <Button withspinner="true" disabled={isUpdatingPassowrd}>
-            {" "}
-            <SpinnerMini /> <span> updating password... </span>{" "}
-          </Button>
-        ) : (
-          <Button withspinner="true"> Save password </Button>
-        )}
-      </Form2>
-
-      <DangerousOperation>
-        <FormRow type="danger" account label={"Password"}>
-          <Input
-            id="delete"
-            type="password"
-            account
-            dangerous="danger"
-            placeholder="••••••••"
-          />
-        </FormRow>
-
-        <Button variation="danger"> Delete my account </Button>
-      </DangerousOperation>
+      <PaswordSettings />
+      <DeleteUserAccount />
     </InfoDetails>
   );
 }
