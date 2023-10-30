6eb7bb0efdb1814268cefb6fb7593b111f06ef5d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-30 09:12:58 +0100

    chore: implemet the password reset compoennt

--- client/src/features/users/PaswordSettings.jsx
+++ client/src/features/users/PaswordSettings.jsx
@@ -1,10 +1,30 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form2 from "../../ui/Form2";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import SpinnerMini from "../../ui/SpinnerMini";
+import { useUpdatePassword } from "./useUpdatePassword";
 
 function PaswordSettings() {
+  const { updatePassword, isUpdatingPassowrd } = useUpdatePassword();
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmitPassword = (
+    { password, passwordConfirm, currentPassword },
+    e
+  ) => {
+    e.preventDefault();
+
+    updatePassword(
+      { currentPassword, password, passwordConfirm },
+      {
+        onSettled: () => reset(),
+      }
+    );
+  };
+
   return (
     <Form2 onSubmit={handleSubmit(onSubmitPassword)}>
       <FormRow account label="Current password">
