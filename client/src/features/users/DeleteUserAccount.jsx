1601beaaee5fe7e8d8d1096616c6b08f0ab19f5e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-30 10:11:53 +0100

    chore: update the delete user account

--- client/src/features/users/DeleteUserAccount.jsx
+++ client/src/features/users/DeleteUserAccount.jsx
@@ -5,6 +5,8 @@ import FormRow from "../../ui/FormRow";
 import { clampBuilder } from "../../styles/clampFunction";
 import { useState } from "react";
 import { useDeleteUser } from "./useDeleteUser";
+import Modal from "../../ui/Modal";
+import DeleteConfirm from "../../ui/DeleteConfirm";
 
 const DangerousOperation = styled.section`
   padding-top: ${() => clampBuilder(320, 1200, 5, 8)};
@@ -31,20 +33,32 @@ function DeleteUserAccount() {
   };
 
   return (
-    <DangerousOperation>
-      <FormRow type="danger" account="true" label={"Password"}>
-        <Input
-          id="delete"
-          type="password"
-          account
-          dangerous="danger"
-          placeholder="••••••••"
-          onChange={(e) => setDeleteInput(e.target.value)}
-        />
-      </FormRow>
+    <Modal>
+      <DangerousOperation>
+        <FormRow type="danger" account="true" label={"Password"}>
+          <Input
+            id="delete"
+            type="password"
+            account
+            dangerous="danger"
+            placeholder="••••••••"
+            onChange={(e) => setDeleteInput(e.target.value)}
+          />
+        </FormRow>
 
-      <Button variation="danger"> Delete my account </Button>
-    </DangerousOperation>
+        <Modal.Open opens="deleteAccount">
+          <Button variation="danger"> Delete my account </Button>
+        </Modal.Open>
+
+        <Modal.Window page="confirmDelete" name="deleteAccount">
+          <DeleteConfirm
+            OnConfirm={onSubmit}
+            resource="account"
+            isDeleting={isDeleting}
+          />
+        </Modal.Window>
+      </DangerousOperation>
+    </Modal>
   );
 }
 
