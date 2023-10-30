9ccdbbc321a5d946bb1f139f55f337e723fcac89
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-30 09:17:53 +0100

    chore: added the delete user component content

--- client/src/features/users/DeleteUserAccount.jsx
+++ client/src/features/users/DeleteUserAccount.jsx
@@ -1,5 +1,38 @@
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+import styled from "styled-components";
+import FormRow from "../../ui/FormRow";
+import { clampBuilder } from "../../styles/clampFunction";
+
+const DangerousOperation = styled.section`
+  padding-top: ${() => clampBuilder(320, 1200, 5, 8)};
+
+  display: flex;
+  flex-direction: column;
+
+  row-gap: ${() => clampBuilder(320, 1200, 1, 2)};
+
+  color: var(--color-red-light);
+
+  font-weight: 600;
+`;
+
 function DeleteUserAccount() {
-  return <div></div>;
+  return (
+    <DangerousOperation>
+      <FormRow type="danger" account label={"Password"}>
+        <Input
+          id="delete"
+          type="password"
+          account
+          dangerous="danger"
+          placeholder="••••••••"
+        />
+      </FormRow>
+
+      <Button variation="danger"> Delete my account </Button>
+    </DangerousOperation>
+  );
 }
 
 export default DeleteUserAccount;
