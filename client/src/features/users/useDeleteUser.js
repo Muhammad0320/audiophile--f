ef4043d350c2424d92141d5e5c03190a877a64c9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-30 09:49:27 +0100

    chore: complete the custom hook to delete user

--- client/src/features/users/useDeleteUser.js
+++ client/src/features/users/useDeleteUser.js
@@ -1,1 +1,22 @@
-export const useDeleteUser = () => {};
+import toast from "react-hot-toast";
+import { deleteUserApi } from "../../service/apiUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteUser, isLoading: isDeleting } = useMutation({
+    mutationFn: deleteUserApi,
+    mutationKey: ["user"],
+
+    onSuccess: () => {
+      toast.success("Your account has been deleted successfylly");
+
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+
+      queryClient.resetQueries({ queryKey: ["user"] });
+    },
+  });
+
+  return { deleteUser, isDeleting };
+};
