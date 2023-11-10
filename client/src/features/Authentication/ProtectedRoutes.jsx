99ce8f08e5295cf3e9ccceb0bd1e1129d692b016
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 12:09:06 +0100

    fix: fix the annoying redirection to login

--- client/src/features/Authentication/ProtectedRoutes.jsx
+++ client/src/features/Authentication/ProtectedRoutes.jsx
@@ -3,8 +3,6 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoutes({ children }) {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  console.log(user);
-
   return user ? children : <Navigate to="/login" replace />;
 }
 
