c40870ebf9b2c8b32fc7cc393518e4f221d103eb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-10 12:05:10 +0100

    chore: used the user in the protected route

--- client/src/features/Authentication/ProtectedRoutes.jsx
+++ client/src/features/Authentication/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
-import { useUser } from "../users/useUser";
 
 function ProtectedRoutes({ children }) {
-  const { user, isLoading } = useUser();
+  const user = JSON.parse(localStorage.getItem("user"));
 
-  return user && !isLoading ? children : <Navigate to="/login" replace />;
+  console.log(user);
+
+  return user ? children : <Navigate to="/login" replace />;
 }
 
 export default ProtectedRoutes;
