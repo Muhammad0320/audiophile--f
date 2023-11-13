77dd24b1c946f02239970facfcd3b21a5441f953
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-13 16:57:28 +0100

    chore: remove the unneccesary part in the client side code that helps in creating order

--- client/src/pages/PaymentConfirmationPage.jsx
+++ client/src/pages/PaymentConfirmationPage.jsx
@@ -3,12 +3,8 @@ import Confirmation from "../ui/Confirmation";
 import ContainerContent from "../ui/ContainerContent";
 
 import ContainerLayout from "../ui/ContainerLayout";
-import useCreateOrder from "../features/payment/useCreateOrder";
-import { useSearchParams } from "react-router-dom";
-import { useEffectOnce } from "../hooks/useeffectOnce";
 
 const Container = styled.div`
-  /* background-color: var(--color-white-vivid); */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,17 +12,6 @@ const Container = styled.div`
 `;
 
 function PaymentConfirmationPage() {
-  const { createOrder } = useCreateOrder();
-
-  const [searchParams] = useSearchParams();
-
-  const data = searchParams.get("session_data");
-  useEffectOnce(() => {
-    if (data) {
-      createOrder({ product: data });
-    }
-  });
-
   return (
     <ContainerLayout>
       <ContainerContent>
