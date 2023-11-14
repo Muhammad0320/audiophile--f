478b78c8f25fd683a9631817c509e9a7062205cf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-14 09:32:56 +0100

    chore: add error boundary comp to the main comp

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "../store.js";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={window.location.replace("/home")}
+      >
+        <App />
+      </ErrorBoundary>
     </Provider>
   </React.StrictMode>
 );
