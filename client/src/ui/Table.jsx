16a5233ac1af37a1a9ba7c09518192a50e2b3907
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-14 12:32:39 +0100

    chore: clean the table comp

--- client/src/ui/Table.jsx
+++ client/src/ui/Table.jsx
@@ -1,11 +1,5 @@
-import { createContext, useContext } from "react";
 import styled from "styled-components";
-import Button from "./Button";
-import { useSendBulkData } from "../features/cart/useSendBulkData";
-import SpinnerMini from "./SpinnerMini";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { clearChanges } from "../features/cart/cartSlice";
+import { createContext, useContext } from "react";
 import { clampBuilder } from "../styles/clampFunction";
 
 // https://dev.to/devland/clean-code-principles-for-javascript-and-typescript-developers-3kdn
@@ -50,7 +44,6 @@ const Header = styled(CommonRow)`
   padding: ${() => clampBuilder(320, 1200, 1, 1.5)};
   ${() => clampBuilder(320, 1200, 1.2, 2)};
   font-size: ${() => clampBuilder(320, 1200, 1.2, 2)};
-  /* background-color: var(--color-white-1); */
 
   font-weight: 600;
   border-bottom: 1px solid var(--color-white-2);
@@ -101,53 +94,11 @@ const TableContext = createContext();
 
 // Create parent element
 
-function Table({ children, column, changes = [], type }) {
-  const { sendBulkdata, isSendingBulkData } = useSendBulkData();
-
-  const dispatch = useDispatch();
-
-  const navigate = useNavigate();
-
-  // send bulk and navigate to checkout
-
-  const handleSendBulk = () => {
-    navigate("/checkout");
-
-    if (changes.length) {
-      sendBulkdata(
-        { changes },
-        {
-          onSuccess: () => {
-            dispatch(clearChanges);
-          },
-        }
-      );
-    }
-  };
-
+function Table({ children, column }) {
   return (
     <TableContext.Provider value={{ column }}>
       <TableAndButtonContainer>
         <TableContainer role="table"> {children}</TableContainer>
-
-        {type === "cart" && changes.length ? (
-          <Button
-            onClick={() => handleSendBulk()}
-            withspinner={isSendingBulkData ? "true" : ""}
-            disabled={isSendingBulkData}
-          >
-            {isSendingBulkData ? (
-              <>
-                {" "}
-                <SpinnerMini /> <span> saving cart data ... </span>{" "}
-              </>
-            ) : (
-              <span> Save cart & checkout </span>
-            )}
-          </Button>
-        ) : (
-          <Button onClick={handleSendBulk}> Checkout </Button>
-        )}
       </TableAndButtonContainer>
     </TableContext.Provider>
   );
