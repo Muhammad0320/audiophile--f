a6f6649ebe7b5c8f8e75a6dd7cd22e145110e564
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-15 15:25:05 +0100

    chore: complete delete order custom hook

--- client/src/features/cart/useDeleteCartOnCheckout.js
+++ client/src/features/cart/useDeleteCartOnCheckout.js
@@ -1,1 +1,19 @@
-export const useDeleteCartOnCheckout = () => {};
+import { useMutation } from "@tanstack/react-query";
+import { deleteCartOnCheckout } from "../../service/apiCart";
+import toast from "react-hot-toast";
+
+export const useDeleteCartOnCheckout = () => {
+  const { mutate: deleteCart, isLoading } = useMutation({
+    mutationFn: deleteCartOnCheckout,
+    mutationKey: ["cart"],
+    onSuccess: () => {
+      toast.success("Order successfully placed");
+    },
+
+    onError: () => {
+      toast.error("An Error occurred while placing your order");
+    },
+  });
+
+  return { deleteCart, isLoading };
+};
