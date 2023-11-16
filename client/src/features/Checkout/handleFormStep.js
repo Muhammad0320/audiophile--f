b9de50f4bc9216b980e6a4e23de5567658e5122e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-16 15:40:38 +0100

    chore: update the handlestepform comp

--- client/src/features/Checkout/handleFormStep.js
+++ client/src/features/Checkout/handleFormStep.js
@@ -1,7 +1,5 @@
-export function handleFormStep(reset, step, setStep) {
+export function handleFormStep(step, setStep) {
   return () => {
-    reset();
-
     if (step <= 3) {
       setStep((step) => step + 1);
     }
