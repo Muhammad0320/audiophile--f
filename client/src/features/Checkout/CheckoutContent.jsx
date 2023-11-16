5dc3d5a1421b880e69a30b7d5d014a01ba485e57
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-16 15:54:19 +0100

    update checkout content

--- client/src/features/Checkout/CheckoutContent.jsx
+++ client/src/features/Checkout/CheckoutContent.jsx
@@ -1,23 +1,23 @@
-import { styled } from "styled-components";
-import ContainerContent from "../../ui/ContainerContent";
-import ContainerLayout from "../../ui/ContainerLayout";
+import Cart from "../cart/Cart";
+import { useState } from "react";
+import Checkout from "./Checkout";
 import Footer from "../../ui/Footer";
 import Header from "../../ui/Header";
-import Cart from "../cart/Cart";
-import BillingDetails from "./BillingDetails";
-import Checkout from "./Checkout";
-import PaymentDetails from "./PaymentDetails";
 import ShippingInfo from "./ShippingInfo";
 import { useSelector } from "react-redux";
+import { styled } from "styled-components";
 import { getCart } from "../cart/cartSlice";
+import BillingDetails from "./BillingDetails";
+import PaymentDetails from "./PaymentDetails";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import ContainerLayout from "../../ui/ContainerLayout";
+import ContainerContent from "../../ui/ContainerContent";
 import { clampBuilder } from "../../styles/clampFunction";
 
 const StyledCheckoutForm = styled.div`
   background-color: var(--color-white);
-  padding: ${() => clampBuilder(320, 1200, 1.8, 2.8)};
-  ${() => clampBuilder(320, 1200, 3, 5)};
+  padding-block: ${() => clampBuilder(320, 1200, 1.8, 2.8)};
+  padding-inline: ${() => clampBuilder(320, 1200, 1.5, 3)};
   border-radius: 1.5rem;
   background-image: var(--color-gradient-dark);
   box-shadow: var(--box-shadow-light);
@@ -42,7 +42,6 @@ function CheckoutContent() {
             <ShippingInfo step={formStep} setStep={setFormStep} />
             <PaymentDetails step={formStep} setStep={setFormStep} />
           </StyledCheckoutForm>
-
           <Cart page="summary" />
         </Checkout>
       </ContainerContent>
