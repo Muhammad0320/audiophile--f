c3257e28c349bc146e69b9ebd0f4c5e27f649feb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-17 18:26:34 +0100

    chore: complete text area comp

--- client/src/ui/TextArea.jsx
+++ client/src/ui/TextArea.jsx
@@ -1,3 +1,23 @@
 import styled from "styled-components";
+import { clampBuilder } from "../styles/clampFunction";
 
-export const TextArea = styled.textarea``;
+export const TextArea = styled.textarea`
+  border: 1px solid var(--color-dark-2);
+  background-color: transparent;
+  padding: ${() => clampBuilder(320, 1200, 0.6, 1)};
+  ${() => clampBuilder(320, 1200, 0.8, 1.6)};
+  caret-color: var(--color-primary);
+  color: var(--color-dark-3);
+  border-radius: 1rem;
+  margin-bottom: ${() => clampBuilder(320, 1200, 1.2, 2)};
+  position: relative;
+
+  &::-webkit-input-placeholder {
+    color: var(--color-dark-3);
+    opacity: 0.7;
+
+    font-size: ${() => clampBuilder(320, 1200, 1.5, 2.5)};
+  }
+
+  resize: none;
+`;
