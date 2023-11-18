f3a0186c567dafc0934babd46d561602cdccf308
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-18 19:14:45 +0100

    chore: create item container

--- client/src/features/Orders/OrderProductDetailsItem.jsx
+++ client/src/features/Orders/OrderProductDetailsItem.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import { clampBuilder } from "../../styles/clampFunction";
+
+const ItemContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1.2fr 1fr;
+
+  background-color: var(--color-white-vivid);
+
+  background-image: var(--color-gradient-dark-muted);
+
+  border-radius: ${() => clampBuilder(320, 1200, 0.5, 1)};
+`;
+
+const Image = styled.img`
+  grid-row: 1 / 2;
+
+  display: block;
+`;
+
 function OrderProductDetailsItem() {
-  return <div></div>;
+  return;
 }
 
 export default OrderProductDetailsItem;
