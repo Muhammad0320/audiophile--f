2a49d740069e8943f9665b818887e12fbc19b39e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-18 19:25:53 +0100

    chore: styled order product detaisl item

--- client/src/features/Orders/OrderProductDetailsItem.jsx
+++ client/src/features/Orders/OrderProductDetailsItem.jsx
@@ -18,8 +18,41 @@ const Image = styled.img`
   display: block;
 `;
 
+const TextBox = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(2, 1fr);
+
+  &:first-child,
+  &:last-child {
+    grid-column: 1 / -1;
+  }
+
+  &:nth-child(3) {
+    grid-column: 1 / 2;
+  }
+
+  &:nth-child(3) {
+    grid-column: 2 / -1;
+  }
+`;
+
+const TextIconContainer = styled.p`
+  display: flex;
+
+  justify-content: center;
+
+  column-gap: ${() => clampBuilder(320, 1200, 0.5, 1)};
+
+  align-items: center;
+`;
+
 function OrderProductDetailsItem() {
-  return;
+  return (
+    <ItemContainer>
+      <Image />
+    </ItemContainer>
+  );
 }
 
 export default OrderProductDetailsItem;
