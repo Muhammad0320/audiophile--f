d7277471e34c2a98352a441815e6c3eb062b22ea
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-20 18:23:12 +0100

    chore: back to prod env

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -2,7 +2,7 @@ export const SHIPPING_FEE = 50;
 
 export const VAT = 0.2;
 
-export const SERVER_ROOT_URL = "http://127.0.0.1:3000/api/v1";
-//   "https://audiophile-muhammad0320.onrender.com/api/v1";
+export const SERVER_ROOT_URL =
+  "https://audiophile-muhammad0320.onrender.com/api/v1";
 
 // "http://127.0.0.1:3000/api/v1";
