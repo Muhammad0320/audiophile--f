c63e6fc4ec9a1a879d67a2b7ffcc56a3589f85e0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-21 18:23:49 +0100

    chore: create class constructor

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,6 +1,27 @@
 const nodemailer = require('nodemailer');
 
-module.exports = class Email {};
+module.exports = class Email {
+  constructor(user, url) {
+    this.url = url;
+    this.firstname = user.name.split(' ')[0];
+    this.from = `Muhammad Awwal <${process.env.EMAIL_FROM}> `;
+    this.to = user.email;
+  }
+
+  // createTransport() {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     return nodemailer.createTransport({
+  //       host: process.env.EMAIL_HOST,
+  //       port: process.env.EMAIL_PORT,
+  //       secure: true,
+  //       auth: {
+  //         pass: process.env.BREVO_PASSWORD,
+  //         user: process.env.BREVO_API_KEY
+  //       }
+  //     });
+  //   }
+  // }
+};
 
 const sendMail = async function(mailOption) {
   const transporter = nodemailer.createTransport({
