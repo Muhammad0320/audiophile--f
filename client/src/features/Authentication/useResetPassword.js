9a33fd8356c1fb5d526eb0ced816964e8f6ebc4c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 17:08:03 +0100

    chore: complete reset password

---
+++ client/src/features/Authentication/useResetPassword.js
@@ -0,0 +1,20 @@
+import toast from "react-hot-toast";
+
+import { useMutation } from "@tanstack/react-query";
+import { resetPasswordApi } from "../../service/apiAuth";
+
+export const useResetPassword = () => {
+  const { isLoading, mutate: resetPassword } = useMutation({
+    mutationFn: resetPasswordApi,
+
+    onSuccess: () => {
+      toast.success("Password reset successful");
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
+  });
+
+  return { isLoading, resetPassword };
+};
