f53d2509611dbbd2a66610711b2d21f7d98b6666
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 14:51:28 +0100

    feat: add reset password page comp

---
+++ client/src/features/Authentication/ResetPassword.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  height: 100dvh;
+  background-color: var(--color-white-2);
+
+  display: grid;
+  place-items: center;
+`;
+
+function ResetPassword() {
+  return <></>;
+}
+
+export default ResetPassword;
