e48b2080e7ba6adf4c6e294fc60ae6e3ffd6485b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 17:41:06 +0100

    chore: navigate to login page on successful password reset

--- client/src/features/Authentication/useResetPassword.js
+++ client/src/features/Authentication/useResetPassword.js
@@ -2,12 +2,18 @@ import toast from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 import { resetPasswordApi } from "../../service/apiAuth";
 
+import { useNavigate } from "react-router-dom";
+
 export const useResetPassword = () => {
+  const navigate = useNavigate();
+
   const { isLoading, mutate: resetPassword } = useMutation({
     mutationFn: resetPasswordApi,
 
     onSuccess: () => {
       toast.success("Password reset successful");
+
+      navigate("/login");
     },
 
     onError: (error) => {
