3328a8e9b11f4586f4365d3cf6b3892c1156a9fa
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 15:19:19 +0100

    chore: complete the password reset page

--- client/src/pages/PasswordResetPage.jsx
+++ client/src/pages/PasswordResetPage.jsx
@@ -1,27 +1,62 @@
 import styled from "styled-components";
+import SVG from "react-inlinesvg";
 
 import { clampBuilder } from "../styles/clampFunction";
+import { IconLogo } from "../ui/Icons";
+import { Heading } from "../ui/Heading";
+import ResetPasswordForm from "../features/Authentication/ResetPasswordForm";
 
 const Container = styled.section`
   height: 100dvh;
-  background-color: var(--color-white-2);
 
   display: grid;
   place-items: center;
+`;
 
+const PasswordResetContainer = styled.div`
   padding-block: ${() => clampBuilder(320, 1200, 2.5, 4.5)};
 
   padding-inline: ${() => clampBuilder(320, 1200, 1.6, 3)};
 
   background-color: var(--color-white-1);
   background-image: var(--color-gradient-light);
+  row-gap: ${() => clampBuilder(320, 1200, 1.4, 3)};
 
   row-gap: ${() => clampBuilder(320, 1200, 1.4, 3)};
   box-shadow: var(--box-shadow-light);
 `;
 
+const StyledSVG = styled.div`
+  justify-self: center;
+
+  & svg {
+    fill: var(--color-dark);
+    transition: all 0.3s;
+
+    &:hover {
+      fill: var(--color-dark-1);
+    }
+  }
+
+  &.active:link svg,
+  &.active:visited svg {
+    fill: var(--color-dark);
+  }
+`;
+
 function PasswordResetPage() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <PasswordResetContainer>
+        <StyledSVG>
+          <SVG src={IconLogo} />
+        </StyledSVG>
+
+        <Heading> Reset your password</Heading>
+        <ResetPasswordForm />
+      </PasswordResetContainer>
+    </Container>
+  );
 }
 
 export default PasswordResetPage;
