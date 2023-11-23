b67b061f19f6ca72c6088502df10a28fc11d987f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 15:42:42 +0100

    chore: useForgot password hook completed

--- client/src/features/Authentication/useForgotPassword.js
+++ client/src/features/Authentication/useForgotPassword.js
@@ -1,1 +1,10 @@
-export const useForgotPassword = () => {};
+import { useMutation } from "@tanstack/react-query";
+import { forgotPasswordApi } from "../../service/apiAuth";
+
+export const useForgotPassword = () => {
+  const { isLoading, mutate: forgotPassword } = useMutation({
+    mutationFn: forgotPasswordApi,
+  });
+
+  return { isLoading, forgotPassword };
+};
