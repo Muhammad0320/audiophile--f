36f30233c3fcaba7fb9a6ef1f1ff5493f5d194d5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 16:15:38 +0100

    chore: add forgot password comp content

--- client/src/features/Authentication/ForgorPassword.jsx
+++ client/src/features/Authentication/ForgorPassword.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 import { clampBuilder } from "../../styles/clampFunction";
+import Form2 from "../../ui/Form2";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+import { useState } from "react";
+import { useForgotPassword } from "./useForgotPassword";
 
 const Container = styled.div`
   display: grid;
@@ -12,12 +18,36 @@ const Container = styled.div`
   background-color: var(--color-white-1);
   background-image: var(--color-gradient-light);
   border-radius: 1.5rem;
-
-  box-shadow: var(--box-shadow-light);
 `;
 
 function ForgorPassword() {
-  return <Container></Container>;
+  const [email, setEmail] = useState("");
+
+  const { forgotPassword, isLoading } = useForgotPassword();
+
+  const handleSubmit = () => {
+    if (!email) return;
+
+    forgotPassword({ email });
+  };
+
+  return (
+    <Container>
+      <Form2 onSubmit={handleSubmit}>
+        <FormRow label="Email" error={!email && "This field is required"}>
+          <Input
+            id="email"
+            placeholder="Enter your email"
+            name="email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormRow>
+
+        <Button> Submit </Button>
+      </Form2>
+    </Container>
+  );
 }
 
 export default ForgorPassword;
