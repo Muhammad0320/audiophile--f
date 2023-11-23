5987e9022bab9df1963e2c26700b24f2a627563c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-23 15:02:36 +0100

    chore: handle password resetn

--- client/src/features/Authentication/ResetPasswordForm.jsx
+++ client/src/features/Authentication/ResetPasswordForm.jsx
@@ -1,4 +1,28 @@
-import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import Form2 from "../../ui/Form2";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
+function ResetPasswordForm() {
+  const { register, reset, handleSubmit } = useForm();
+
+  return (
+    <Form2>
+      <FormRow label="New Password">
+        <Input id="password" type="password" placeholder="••••••••" />
+      </FormRow>
+
+      <FormRow label="Confirm New Password">
+        <Input id="password" type="password" placeholder="••••••••" />
+      </FormRow>
+    </Form2>
+  );
+}
+
+export default ResetPasswordForm;
+
+/* 
+
 
 const Container = styled.section`
   height: 100dvh;
@@ -13,3 +37,6 @@ function ResetPassword() {
 }
 
 export default ResetPassword;
+
+
+*/
