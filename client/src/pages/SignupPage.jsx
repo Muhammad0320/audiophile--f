6585fd2508182f935b295d38365b502b14ef3d87
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-10 19:26:50 +0100

    chroe: update signup comp

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,64 +1,26 @@
-import styled from "styled-components";
-
 import SVG from "react-inlinesvg";
 import { IconLogo } from "../ui/Icons";
 import SignupForm from "../features/Authentication/SignupForm";
 import OtherAuthmethod from "../ui/OtherAuthmethod";
-import { clampBuilder } from "../styles/clampFunction";
+
 import { Heading } from "../ui/Heading";
-
-const PageContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: grid;
-  justify-content: center;
-  align-items: center;
-  padding-block: ${() => clampBuilder(320, 1200, 3, 6)};
-  overflow-y: scroll;
-`;
-
-const StyledSignupContainer = styled.div`
-  display: grid;
-  justify-content: center;
-  align-items: center;
-  padding: ${() => clampBuilder(320, 1200, 2.5, 4.5)};
-  ${() => clampBuilder(320, 1200, 1.3, 2.5)};
-  background-color: var(--color-white-1);
-  background-image: var(--color-gradient-light);
-  border-radius: 1.5rem;
-  row-gap: ${() => clampBuilder(320, 1200, 1.4, 3)};
-  box-shadow: var(--box-shadow-light);
-`;
-
-const StyledSVG = styled.div`
-  justify-self: center;
-
-  & svg {
-    fill: var(--color-dark);
-    transition: all 0.3s;
-
-    &:hover {
-      fill: var(--color-dark-1);
-    }
-  }
-
-  &.active:link svg,
-  &.active:visited svg {
-    fill: var(--color-dark);
-  }
-`;
+import {
+  PageContainer,
+  StyledAuthContainer,
+  StyledSVG,
+} from "../ui/AuthPageStyles";
 
 function SignupPage() {
   return (
     <PageContainer>
-      <StyledSignupContainer>
+      <StyledAuthContainer>
         <StyledSVG>
           <SVG src={IconLogo} />
         </StyledSVG>
         <Heading type="login"> Log in to your account </Heading>
         <SignupForm />
         <OtherAuthmethod authMethod="sign in" />
-      </StyledSignupContainer>
+      </StyledAuthContainer>
     </PageContainer>
   );
 }
