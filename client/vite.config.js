ab07012ce0c368aa07f7ecd2f03e97ea4309f13e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-16 09:12:08 +0100

    fix: try to fix vite problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
+import { AutoImport } from "unplugin-auto-import";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "127.0.0.1",
   },
+  plugins: [
+    react(),
+    eslint(),
+    AutoImport({
+      imports: [
+        "react",
+        "react-dom",
+        "react-router-dom",
+        "react-navigation",
+        // other libraries you want to auto import
+      ],
+      dts: "src/auto-imports.d.ts",
+    }),
+  ],
 
-  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 });
