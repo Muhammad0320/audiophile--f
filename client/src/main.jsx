493e83bee33a0d9692ad21b1878d48db448801ca
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-31 10:03:34 +0100

    chore:  update use comp

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
 import store from "../store.js";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./pages/ErrorFallback.jsx";
 
