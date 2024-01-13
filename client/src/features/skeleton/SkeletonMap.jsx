900ae754f762e87ba8ffb2d31d9f7e593dac5d83
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-13 06:20:23 +0100

    chore: update skeleton map comp

--- client/src/features/skeleton/SkeletonMap.jsx
+++ client/src/features/skeleton/SkeletonMap.jsx
@@ -1,10 +1,16 @@
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-function SkeletonMap({ count = 3, children }) {
+function SkeletonMap({ count = 3, style }) {
   const skeletonCount = Array(count).fill(0);
 
-  return <>{skeletonCount.map((_, i) => children)}</>;
+  return (
+    <>
+      {skeletonCount.map((_, i) => (
+        <Skeleton style={style} />
+      ))}
+    </>
+  );
 }
 
 export default SkeletonMap;
