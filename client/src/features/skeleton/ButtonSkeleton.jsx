76c97eef85866e2073c402bf39e7dc25dc07ec9f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-13 05:49:22 +0100

    chore: update button code name

--- client/src/features/skeleton/ButtonSkeleton.jsx
+++ client/src/features/skeleton/ButtonSkeleton.jsx
@@ -1,7 +1,7 @@
 import { styled } from "styled-components";
 import { clampBuilder } from "../../styles/clampFunction";
 
-export const ButtonContainer = styled.div`
+export const ButtonSkeleton = styled.div`
   height: 4.5rem;
   width: 7.5rem;
   border-radius: ${() => clampBuilder(320, 1200, 0.4, 1)};
